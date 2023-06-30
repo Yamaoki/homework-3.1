d460e3789bdac5441547f4fa47f72ee57cdf5430
Андрей <d89284146236sv@gmail.com>
2023-06-30 21:13:29 +0300

    проверьте дз

--- src/main/java/ru/hogwarts/school/model/Student.java
+++ src/main/java/ru/hogwarts/school/model/Student.java
@@ -5,38 +5,60 @@ import jakarta.persistence.*;
 import java.util.Objects;
 
 @Entity
-@Table(name = "Студенты")
 public class Student {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    @GeneratedValue
+    private Long id;
     private String name;
     private int age;
 
+    @ManyToOne
+    @JoinColumn(name = "faculty_id")
+    private Faculty faculty;
+
+
     public Student() {
     }
-
-    public Student(long id, String name, int age) {
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
         this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
         this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
         this.age = age;
+    }
+
+    public Faculty getFaculty() {
+        return faculty;
+    }
+
+    public void setFaculty(Faculty faculty) {
+        this.faculty = faculty;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o)
             return true;
-        if (o == null || getClass() != o.getClass())
+        if (!(o instanceof ru.hogwarts.school.model.Student))
             return false;
-        Student student = (Student) o;
-        return id == student.id && age == student.age && Objects.equals(name, student.name);
+        ru.hogwarts.school.model.Student student = (ru.hogwarts.school.model.Student) o;
+        return age == student.age && Objects.equals(id, student.id) && Objects.equals(name, student.name);
     }
-
     @Override
     public int hashCode() {
         return Objects.hash(id, name, age);
     }
-
     @Override
     public String toString() {
         return "Student{" +
@@ -44,29 +66,5 @@ public class Student {
                 ", name='" + name + '\'' +
                 ", age=" + age +
                 '}';
+        }
     }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-}
--- src/main/java/ru/hogwarts/school/service/StudentService.java
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -3,16 +3,16 @@ package ru.hogwarts.school.service;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.exception.FacultyNotFoundException;
 import ru.hogwarts.school.exception.StudentNotFoundException;
+import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
 import ru.hogwarts.school.repository.StudentRepository;
 
 import java.util.Collection;
+import java.util.List;
 
 @Service
 public class StudentService {
-
     private final StudentRepository studentRepository;
-
     public StudentService(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
     }
@@ -38,5 +38,15 @@ public class StudentService {
                 .orElseThrow(() -> new FacultyNotFoundException(id));
         studentRepository.deleteById(id);
         return student;
+    }
+
+    public List<Student> getStudentsByAgeBetween(int ageFrom, int ageTo) {
+        return studentRepository.getStudentsByAgeBetween(ageFrom, ageTo);
+    }
+
+    public Faculty getFacultyByStudentId(Long id) {
+        Student student = studentRepository.findById(id)
+                .orElseThrow(() -> new FacultyNotFoundException(id));
+        return student.getFaculty();
     }
 }

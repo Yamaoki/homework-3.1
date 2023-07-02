0c4cca274f81e804224f53cccfb95df71e857371
Андрей <d89284146236sv@gmail.com>
2023-07-02 22:16:59 +0300

    проверьте дз

--- src/main/java/ru/hogwarts/school/service/StudentService.java
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -1,20 +1,33 @@
 package ru.hogwarts.school.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import ru.hogwarts.school.exception.FacultyNotFoundException;
 import ru.hogwarts.school.exception.StudentNotFoundException;
+import ru.hogwarts.school.model.Avatar;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.repository.AvatarRepository;
 import ru.hogwarts.school.repository.StudentRepository;
 
-import java.util.Collection;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
+import java.util.Objects;
+
+import static java.nio.file.StandardOpenOption.CREATE_NEW;
 
 @Service
 public class StudentService {
+    @Value("${avatars.dir.path}")
+    private String avatarsDir;
     private final StudentRepository studentRepository;
-    public StudentService(StudentRepository studentRepository) {
+    private final AvatarRepository avatarRepository;
+    public StudentService(StudentRepository studentRepository, AvatarRepository avatarRepository) {
         this.studentRepository = studentRepository;
+        this.avatarRepository = avatarRepository;
     }
     public Student addStudent(Student student) {
         return studentRepository.save(student);
@@ -22,9 +35,7 @@ public class StudentService {
     public Student getStudentById(Long id) {
         return studentRepository.findById(id).orElseThrow(() -> new FacultyNotFoundException(id));
     }
-    public Collection<Student> getAllStudents() {
-        return studentRepository.findAll();
-    }
+
     public Student updateStudent(Long id, Student student) {
         studentRepository.findById(id).orElseThrow(() -> new StudentNotFoundException(id));
         Student oldStudent = new Student();
@@ -33,11 +44,8 @@ public class StudentService {
         oldStudent.setName(student.getName());
         return studentRepository.save(oldStudent);
     }
-    public Student deleteStudentById(Long id) {
-        Student student = studentRepository.findById(id)
-                .orElseThrow(() -> new FacultyNotFoundException(id));
+    public void deleteStudentById(Long id) {
         studentRepository.deleteById(id);
-        return student;
     }
 
     public List<Student> getStudentsByAgeBetween(int ageFrom, int ageTo) {
@@ -48,5 +56,44 @@ public class StudentService {
         Student student = studentRepository.findById(id)
                 .orElseThrow(() -> new FacultyNotFoundException(id));
         return student.getFaculty();
+    }
+    public Student getStudentInfo(Long id) {
+        studentRepository.findById(id).orElseThrow(() -> new StudentNotFoundException(id));
+        Student student = new Student();
+        student.getId();
+        student.getAge();
+        student.getName();
+        return student;
+    }
+    public Avatar findAvatar(long studentId) {
+        return avatarRepository.findByStudentId(studentId).orElseThrow();
+    }
+
+    public void uploadAvatar(Long studentId, MultipartFile file) throws IOException {
+        Student student = getStudentById(studentId);
+        Path filePath = Path.of(avatarsDir, studentId + "." + getExtension(Objects.requireNonNull(file.getOriginalFilename())));
+        Files.createDirectories(filePath.getParent());
+        Files.deleteIfExists(filePath);
+
+        try (InputStream is = file.getInputStream();
+             OutputStream os = Files.newOutputStream(filePath, CREATE_NEW);
+             BufferedInputStream bis = new BufferedInputStream(is, 1024);
+             BufferedOutputStream bos = new BufferedOutputStream(os, 1024)
+        ) {
+            bis.transferTo(bos);
+        }
+
+        Avatar avatar = avatarRepository.findByStudentId(studentId).orElseGet(Avatar::new);
+        avatar.setStudent(student);
+        avatar.setFilePath(filePath.toString());
+        avatar.setFileSize(file.getSize());
+        avatar.setMediaType(file.getContentType());
+        avatar.setData(file.getBytes());
+
+        avatarRepository.save(avatar);
+    }
+
+    private String getExtension(String fileName) {
+        return fileName.substring(fileName.lastIndexOf(".") + 1);
     }
 }

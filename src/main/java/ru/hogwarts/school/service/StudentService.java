1d85002d440ed2ab12adc5dcf4b1a1ed8cf83165
Андрей <d89284146236sv@gmail.com>
2023-07-23 19:18:30 +0300

    проверьте дз

--- src/main/java/ru/hogwarts/school/service/StudentService.java
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -11,6 +11,7 @@ import ru.hogwarts.school.model.Student;
 import ru.hogwarts.school.repository.AvatarRepository;
 import ru.hogwarts.school.repository.StudentRepository;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Locale;
@@ -19,6 +20,7 @@ import java.util.Locale;
 public class StudentService {
     private final StudentRepository studentRepository;
     private final AvatarRepository avatarRepository;
+    private Integer currentStudent = 0;
     private final Logger logger = LoggerFactory.getLogger(StudentService.class);
     public StudentService(StudentRepository studentRepository, AvatarRepository avatarRepository) {
         this.studentRepository = studentRepository;
@@ -107,5 +109,66 @@ public class StudentService {
                 .mapToInt(e -> e.getAge())
                 .average()
                 .orElse(0);
+    }
+    public void printFewStudentsThreads() {
+        logger.info("Was invoked method for print few students in different threads");
+        List<Student> students = new ArrayList<>(this.getAllStudents());
+
+        new Thread(() -> {
+            printStudent(students, "Поток 1", 2);
+            printStudent(students, "Поток 1", 3);
+        }).start();
+
+        new Thread(() -> {
+            printStudent(students, "Поток 2", 4);
+            printStudent(students, "Поток 2", 5);
+        }).start();
+
+        printStudent(students, "Поток 0", 0);
+        printStudent(students, "Поток 0", 1);
+    }
+
+    private void printStudent(List<Student> students, String message, int number) {
+        System.out.println(message + ": #" + number + ": " + students.get(number).getName());
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void printFewStudentsThreadsSync() {
+        logger.info("Was invoked method for print few students in different synchronized threads");
+        List<Student> students = new ArrayList<>(this.getAllStudents());
+
+        Thread thread1 = new Thread(() -> {
+            printStudentSync(students, "Поток 1", 2);
+            printStudentSync(students, "Поток 1", 3);
+        });
+        thread1.start();
+
+        Thread thread2 = new Thread(() -> {
+            printStudentSync(students, "Поток 2", 4);
+            printStudentSync(students, "Поток 2", 5);
+        });
+        thread2.start();
+
+        printStudentSync(students, "Поток 0", 0);
+        printStudentSync(students, "Поток 0", 1);
+    }
+
+    private void printStudentSync(List<Student> students, String message, int number) {
+        while (currentStudent != number) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        synchronized (currentStudent) {
+            System.out.println(message + ": #" + number + ": " + students.get(number).getName());
+            currentStudent++;
+        }
     }
 }
